53df5867c815a19c0dbcb24b1dbf608ff42503be
The-Vuppukari <mohansai.2905@gmail.com>
2020-09-14 11:17:31 +0530

    Add : A program to find the number of times the heads won or the tails won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,12 +2,20 @@
 
 isHeads=0
 isTails=1
-coinFlip=$((RANDOM%2))
 
-if [ $isHeads -eq $coinFlip ]
-then
-	print "Heads"
-elif [ $isTails -eq $coinFlip ]
-then
-	print "Tails"
-fi
+heads=0
+tails=0
+
+for (( i=1; i<=100; i++ ))
+do
+	coinFlip=$((RANDOM%2))
+	if [ $isHeads -eq $coinFlip ]
+	then
+		((heads++))	
+	elif [ $isTails -eq $coinFlip ]
+	then
+		((tails++))
+	fi
+done
+echo "Number of times heads won = $heads "
+echo "Number of times tails won = $tails "
