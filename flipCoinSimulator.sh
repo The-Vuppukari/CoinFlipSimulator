41986a0c92dffb544d58e47718d9fa11482304f6
The-Vuppukari <mohansai.2905@gmail.com>
2020-09-15 12:23:16 +0530

    Refactor The code wether its a tie are not is won with how much

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,20 +2,30 @@
 
 isHeads=0
 isTails=1
+finalCount=21
 
 heads=0
 tails=0
 
-for (( i=1; i<=100; i++ ))
+while [[ $heads -lt $finalCount ]] && [[ $tails -lt $finalCount ]]
 do
 	coinFlip=$((RANDOM%2))
-	if [ $isHeads -eq $coinFlip ]
+	if [[ $isHeads -eq $coinFlip ]]
 	then
 		((heads++))	
-	elif [ $isTails -eq $coinFlip ]
+	elif [[ $isTails -eq $coinFlip ]]
 	then
 		((tails++))
 	fi
 done
-echo "Number of times heads won = $heads "
-echo "Number of times tails won = $tails "
+
+if [[ $heads -eq $finalCount ]] && [[ $tails -eq $finalCount ]]
+then
+	echo "Its a tie"
+elif [[ $heads -eq $finalCount ]]
+then
+	echo "Heads won the by $(( $heads-$tails ))"
+elif [[ $tails -eq $finalCount ]]
+then
+	echo "Tails won by $(( $tails-$heads ))"
+fi
